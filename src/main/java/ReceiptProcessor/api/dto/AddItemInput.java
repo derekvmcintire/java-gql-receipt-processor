3cd4fdcdc76d2e6e0e6a14c5d27fc591e15c882f
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2024-12-02 12:35:56 -0500

    Use lombok to add dto methods (#5)
    Co-authored-by: Derek McIntire <derekbmcintire@gmail.com>

--- src/main/java/ReceiptProcessor/api/dto/AddItemInput.java
+++ src/main/java/ReceiptProcessor/api/dto/AddItemInput.java
@@ -1,74 +1,20 @@
 package ReceiptProcessor.api.dto;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class AddItemInput {
   private String name;
   private int quantity;
   private double price;
-
-  // Default constructor
-  public AddItemInput() {
-  }
-
-  // Parameterized constructor
-  public AddItemInput(String name, int quantity, double price) {
-    this.name = name;
-    this.quantity = quantity;
-    this.price = price;
-  }
-
-  // Getters
-  public String getName() {
-    return name;
-  }
-
-  public int getQuantity() {
-    return quantity;
-  }
-
-  public double getPrice() {
-    return price;
-  }
-
-  // Setters
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setQuantity(int quantity) {
-    this.quantity = quantity;
-  }
-
-  public void setPrice(double price) {
-    this.price = price;
-  }
-
-  // toString method for debugging
-  @Override
-  public String toString() {
-    return "AddItemInput{" +
-        "name='" + name + '\'' +
-        ", quantity=" + quantity +
-        ", price=" + price +
-        '}';
-  }
-
-  // equals and hashCode for object comparison (optional but recommended)
-  @Override
-  public boolean equals(Object o) {
-    if (this == o)
-      return true;
-    if (o == null || getClass() != o.getClass())
-      return false;
-    AddItemInput that = (AddItemInput) o;
-    return quantity == that.quantity &&
-        Double.compare(that.price, price) == 0 &&
-        Objects.equals(name, that.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, quantity, price);
-  }
 }
