1bcb482ca73c7842f7d2f10554174bfaf3cc6ddd
Derek McIntire <derekbmcintire@gmail.com>
2024-12-05 20:54:26 -0500

    Refactored rules

--- src/main/java/ReceiptProcessor/application/utility/PointsCalculator.java
+++ src/main/java/ReceiptProcessor/application/utility/PointsCalculator.java
@@ -4,22 +4,54 @@ import org.springframework.stereotype.Component;
 
 import ReceiptProcessor.api.dto.AddReceiptInput;
 
-@Component
+/**
+ * PointsCalculator is responsible for calculating points based on a given
+ * receipt.
+ * 
+ * It uses a list of rules to determine how many points to assign to a receipt.
+ * These rules are retrieved from the RuleRegistry, which holds all Rule beans
+ * (i.e., classes that implement the Rule interface and are annotated
+ * with @Component).
+ * 
+ * Spring will automatically inject the RuleRegistry bean, which contains all
+ * the rules,
+ * so new rules can be added or modified without needing to change this class.
+ */
+@Component // Marks this class as a Spring-managed component for dependency injection.
 public class PointsCalculator {
-  private final PointsCalculatorRules rules;
 
-  public PointsCalculator(
-      PointsCalculatorRules pointsCalculatorRules) {
-    this.rules = pointsCalculatorRules;
+  // The RuleRegistry is injected automatically by Spring. It provides access to
+  // all Rule beans.
+  private final RuleRegistry ruleRegistry;
+
+  /**
+   * Constructor for PointsCalculator.
+   * 
+   * @param ruleRegistry An instance of RuleRegistry, automatically injected by
+   *                     Spring.
+   *                     It holds a list of all the Rule implementations.
+   */
+  public PointsCalculator(RuleRegistry ruleRegistry) {
+    this.ruleRegistry = ruleRegistry; // Assign the injected RuleRegistry to the private field.
   }
 
+  /**
+   * Calculates the total points for the given receipt by applying all registered
+   * rules.
+   * 
+   * This method iterates through all rules in the RuleRegistry and applies each
+   * one
+   * to the provided receipt. The results from all rules are summed to get the
+   * total points.
+   * 
+   * @param receipt The receipt for which the points are calculated.
+   * @return The total points calculated based on the rules.
+   */
   public int calculatePoints(AddReceiptInput receipt) {
-    return rules.addPointsForRetailName(receipt)
-        + rules.addPointsForRoundDollarTotal(receipt)
-        + rules.addPointsForMultipleOfQuarter(receipt)
-        + rules.addPointsForItemCount(receipt)
-        + rules.addPointsForItemDescriptions(receipt)
-        + rules.addPointsForOddPurchaseDate(receipt)
-        + rules.addPointsForPurchaseTime(receipt);
+    return ruleRegistry.getRules() // Retrieves the list of rules from RuleRegistry.
+        .stream() // Converts the list to a Stream to apply operations on each rule.
+        .mapToInt(rule -> rule.calculate(receipt)) // For each rule, apply the calculate method and get the result as an
+                                                   // int.
+        .sum(); // Sums up the results of all rules to get the total points.
   }
 }
